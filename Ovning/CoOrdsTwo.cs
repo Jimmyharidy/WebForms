42d9a1bf9ce2e2bc9c422fc0cf5c5303fa7b769c
Jimmyharidy <jimmy.haridy@hotmail.com>
2016-01-08 09:51:05 +0100

    Ovning added

---
+++ Ovning/CoOrdsTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning
+{
+    public partial class CoOrds
+    {
+        public void PrintCoOrds()
+        {
+            System.Console.WriteLine("CoOrds:{0}, {1}", x, y);
+        }
+    }
+}
---
+++ Ovning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning
+{
+    class Program
+    {
+        public partial class CoOrds
+        {
+            private int x;
+            private int y;
+
+            public CoOrds(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        class TestCoOrds
+        {
+            static void Main()
+            {
+               CoOrds myCoOrds = new CoOrds(10, 15);
+                myCoOrds.PrintCoOrds();
+            }
+        }
+    }
+}
